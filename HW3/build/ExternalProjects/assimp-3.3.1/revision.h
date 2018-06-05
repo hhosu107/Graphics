8930317a6705dcedfd4c35230ff9b67814abfa53
Chansu Park <hhosu107@gmail.com>
2018-06-05 21:00:32 +0900

    Remove unintended builds

--- HW3/build/ExternalProjects/assimp-3.3.1/revision.h
+++ HW3/build/ExternalProjects/assimp-3.3.1/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x761f41c
+#define GitVersion 0xa7010ed
 #define GitBranch "outline"
 
 #endif // ASSIMP_REVISION_H_INC
